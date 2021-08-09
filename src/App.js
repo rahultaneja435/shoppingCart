47a7e5a6a8ded02a4ae59801d5688695e33b3fa6
Rahul Taneja <rahultaneja03@gmail.com>
2021-08-09 19:02:55 +0530

    added commerce.js dependency and adding product dynamically from an API and some comments too

--- src/App.js
+++ src/App.js
@@ -2,8 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import Products from './Components/Products/Products'
 import Navbar from './Components/Navbar/Navbar';
+import {commerce} from './Commerce/Commerce'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [products,setProducts] = useState([]); // using this for fetching products list from commerce API
+  const productFetcher = async()=>
+  {
+    
+    const result = await commerce.products.list(); //promise will be received
+    console.log(result);
+    setProducts(result);
+  }
+  useEffect(()=>
+  {
+   productFetcher();
+  },[])
   return (
     <div className="App">
       <Products/>
