a1be0fef9f502b496be04315b296f47e70826757
Rahul Taneja <rahultaneja03@gmail.com>
2021-08-09 19:03:53 +0530

    added comment

--- src/Commerce/Commerce.js
+++ src/Commerce/Commerce.js
@@ -1,3 +1,3 @@
 import Commerce from '@chec/commerce.js'
 
-export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY,true);
+export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY,true); // this is for using commerce API used for getting products,adding it to cart and all the shopping experience
