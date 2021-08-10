bca64ec8cf1e2c948b52400e2c57b5b85ad43a22
Rahul Taneja <rahultaneja03@gmail.com>
2021-08-10 14:57:57 +0530

    added router and button functionalities for Carts

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
 import {AppBar,Toolbar,IconButton,badge,MenuItem,Menu,Typography, Badge} from '@material-ui/core'
 import {ShoppingCart} from '@material-ui/icons'
+import {Link,useLocation} from 'react-router-dom'
 import useStyles from '../Products/styles'
 function Navbar({cartLength}) {
     const classes = useStyles() ;
+    const location = useLocation();
     return (
         <div>
             <AppBar position="fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
-                   <Typography variant="h6" className={classes.title} color="inherit">
+                   <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
                        <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmtPddxOs2WNkToEkviq8v8_R88NxSWzRzuQ&usqp=CAU"} alt="Intech" height="25px" className={classes.image}/>
                        Money heist Store
                    </Typography>
                    <div className={classes.grow}>
-                       <div className={classes.button}>
-                           <IconButton aria-label="Show Cart Items" color="inherit">
+                   {location.pathname==='/' && (<div className={classes.button}>
+                           <Link to="/cart">go to cart</Link>
+                           <IconButton component={Link} aria-label="Show Cart Items" color="inherit">
                                 <Badge badgeContent={cartLength} color="secondary">
                                     <ShoppingCart/>
                                 </Badge>
                            </IconButton>
-                       </div>
+                       </div>)}
                    </div>
                 </Toolbar>
             </AppBar>
